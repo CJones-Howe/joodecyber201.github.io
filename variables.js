519fc9bf34e1b12d5c8e77fa07f57fbfc3d65893
Joode825 <jofori@albany.edu>
2025-04-01 00:29:27 -0400

    variables.js

--- variables.js
+++ variables.js
@@ -1,13 +1,13 @@
 //using let
-let name = "Rohan";
-let age = "18";
+let name = "Jude";
+let age = "19";
 let isStudent = "True";
 
 document.getElementById("demo1").innerHTML =
 "<br>Name: " + name + "<br>Age: " + age + "<br>Student: " + isStudent;
 
-let favColor = "Red";
-let favFood = "Burger";
+let favColor = "Purple";
+let favFood = "Wings";
 let favSport = "Basketball";
 
 document.getElementById("demo2").innerHTML =
